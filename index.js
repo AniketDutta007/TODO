b74cff028b7f909d1bc97ae94aff0e21f72542ef
Aniket Dutta <aniket2018dutta@gmail.com>
2021-11-24 06:06:08 +0530

    set the file structure and initiate the server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express'); // importing the express module
+const app = express(); // firing the express server
+const port = 8000; // assinging port
+
+app.listen(port, function(err){ // callback function to determine server status
+    if(err){
+        console.log(`Error in running the SERVER : ${err}`);
+        return;
+    }
+    console.log(`Server is up and running on port ${port}`);
+});
