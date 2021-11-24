a8fd35987267d4e4a019f5cdb46cda8b79873bcc
Aniket Dutta <aniket2018dutta@gmail.com>
2021-11-24 06:12:43 +0530

    setup router and controller for home

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express'); // importing the express module
 const app = express(); // firing the express server
 const port = 8000; // assinging port
 
+app.use('/', require('./routers/index'));
+
 app.listen(port, function(err){ // callback function to determine server status
     if(err){
         console.log(`Error in running the SERVER : ${err}`);
